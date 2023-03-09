b66ef79137f879ecd8360a548c476f410514e31e
Konstantinos Georgoudis <k.georgoudis@pan-net.eu>
2023-03-09 14:08:12 +0200

    demo changes

--- loop.sh
+++ loop.sh
@@ -1,4 +1,5 @@
-for ((i=1;i<=100;i++)); do
-    curl -sI -o /dev/null -w "%{http_code}\n" https://www.google.com;
+while true; do
+    curl -sI -o /dev/null -w "%{http_code}\n" https://www.blacklines.gr/how-to/;
+    curl -sI -o /dev/null -w "%{http_code}\n" https://www.blacklines.gr/code/;
     sleep 2;
 done
