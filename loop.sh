cceec8c11952a6b907c4a2caac43771a8e1f1bf9
Konstantinos Georgoudis <k.georgoudis@pan-net.eu>
2023-03-09 14:20:09 +0200

    demo changes

--- loop.sh
+++ loop.sh
@@ -1,5 +1,5 @@
 while true; do
-    curl -sI -o /dev/null -w "%{http_code}\n" https://www.blacklines.gr/how-to/;
-    curl -sI -o /dev/null -w "%{http_code}\n" https://www.blacklines.gr/code/;
+    curl -sI -o /dev/null -w "%{http_code}\n" -H "Host: www.blacklines.gr" https://www.blacklines.gr/how-to/;
+    curl -sI -o /dev/null -w "%{http_code}\n" -H "Host: www.blacklines.gr" https://www.blacklines.gr/code/;
     sleep 2;
 done
